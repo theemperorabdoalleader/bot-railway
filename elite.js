7cf6ec98fce164f83cdc995311c1f042a4d5916c
theemperorabdoalleader <theemperorabdoalleader@gmail.com>
2026-07-09 02:52:47 +0300

    Implement add function for elite users

---
+++ elite.js
@@ -0,0 +1,12 @@
+const { db } = require('./db');
+const { normalizeJid } = require('./utils');
+const config = require('./config');
+
+async function add(sock, from, sender, args) {
+    if (!config.owner.includes(normalizeJid(sender))) return sock.sendMessage(from, { text: 'للاونر فقط' });
+    const target = normalizeJid(args[0]);
+    if (!db.elite.includes(target)) db.elite.push(target);
+    sock.sendMessage(from, { text: 'تم اضافة للنخبة ✅' });
+}
+
+module.exports = { add };
